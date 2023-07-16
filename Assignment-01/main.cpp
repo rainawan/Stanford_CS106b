3afe01d0809432f7ca96b1eaf3ee599d03573a7d
Raina Wan <102885970+rainawan@users.noreply.github.com>
2023-07-15 22:28:21 -0700

    Create main.cpp

---
+++ Assignment-01/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "console.h"
+#include "SimpleTest.h"
+#include "perfect.h"
+#include "soundex.h"
+using namespace std;
+
+
+int main() {
+    if (runSimpleTests(SELECTED_TESTS)) {
+        return 0;
+    }
+
+    findPerfects(40000);
+    soundexSearch("res/surnames.txt");
+
+    cout << endl << "main() completed." << endl;
+    return 0;
+}
+
+
+// Do not remove or edit below this line. It is here to tom confirm that your code
+// conforms to the expected function prototypes needed for grading
+void confirmFunctionPrototypes() {
+    long n = 0;
+    bool b;
+    string s;
+
+    n = divisorSum(n);
+    b = isPerfect(n);
+    findPerfects(n);
+
+    n = smarterSum(n);
+    b = isPerfectSmarter(n);
+    findPerfectsSmarter(n);
+
+    n = findNthPerfectEuclid(n);
+
+    s = lettersOnly(s);
+    s = soundex(s);
+    soundexSearch(s);
+}
